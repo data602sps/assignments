df86ddf09bcbea43c8e858a195f7f629f8364f2b
data602sps <60321106+data602sps@users.noreply.github.com>
2020-02-08 14:57:44 -0500

    Create 02_assignment.py

---
+++ 02_assignment.py
@@ -0,0 +1,235 @@
+'''
+Assignment #2
+1. Add / modify code ONLY between the marked areas (i.e. "Place code below")
+2. Run the associated test harness for a basic check on completeness. A successful run of the test cases does not 
+    guarantee accuracy or fulfillment of the requirements. Please do not submit your work if test cases fail.
+3. To run unit tests simply use the below command after filling in all of the code:
+    python 07_assignment.py
+  
+4. Unless explicitly stated, please do not import any additional libraries but feel free to use built-in Python packages
+5. Submissions must be a Python file and not a notebook file (i.e *.ipynb)
+6. Do not use global variables unless stated to do so
+7. Make sure your work is committed to your master branch in Github
+Installation requirements:
+1. Please install numpy: pip install numpy
+'''
+import math
+import unittest
+import numpy as np
+import requests as r
+
+def exercise01():
+    # Create a list called animals containing the following animals: cat, dog, crouching tiger, hidden dragon, manta ray
+
+    # ------ Place code below here \/ \/ \/ ------
+
+
+    # ------ Place code above here /\ /\ /\ ------
+
+    return animals
+
+
+def exercise02():
+    # Repeat exercise 1 and loop through and print each item in the animal list by iterating through an index number and using range(). Set the variable len_animals to the length of the animal list.
+
+    # ------ Place code below here \/ \/ \/ ------
+
+
+    # ------ Place code above here /\ /\ /\ ------
+
+    return animals, len_animals
+
+
+def exercise03():
+    # Programmatically reorganize the countdown list below in descending order and return the value of the 5th element in the sorted countdown list.
+    # The 5th element will be stored in the variable the_fifth_element, which currently below has a dummy value of -999.
+    # Remember, the index number of the 5th element is not 5
+    
+    countdown = [9, 8, 7, 5, 4, 2, 1, 6, 10, 3, 0, -5]
+    the_fifth_element = -999
+
+    # ------ Place code below here \/ \/ \/ ------
+
+
+    # ------ Place code above here /\ /\ /\ ------
+
+    return countdown, the_fifth_element
+
+
+def exercise04(more_temperatures, iot_sensor_points, a, b, c, d, e):
+    # This exercise function receives a list of temperatures and a dictionary of temperature data where the key is an arbitrary sequential number and the value is the temperature and a,b,c,d and e are each a single temperature reading
+    # To Do:
+    # 1. Add all of the items in more_temperatures to the temperatures list
+    # 2. Add all of the temperature values in iot_sensor_points to the temperatures list
+    # 3. Add a,b,c,d and e to the temperature list
+    # 4. Organize the temperatures in descending order
+    # 5. The samples list will contain every 5th reading from the final temperatures list i.e in list [1,2,3,4,5,6,7,8,9,10] samples would be [5,10]
+    # 6. Do a shallow copy of samples into copy_of_samples
+    # 7. Organize samples in ascending order
+
+    temperatures = list(np.random.randint(-25, 25, size=10))
+    samples = []
+    copy_of_samples = []
+
+    # ------ Place code below here \/ \/ \/ ------
+
+
+
+    # ------ Place code above here /\ /\ /\ ------
+
+    return samples, temperatures, more_temperatures, iot_sensor_points, a, b, c, d, e, copy_of_samples
+
+
+def exercise05(n):
+    # This function will find n factorial using recursion (calling itself) and return the solution. For example exercise05(5) will return 120. No Python functions are to be used.
+
+    # ------ Place code below here \/ \/ \/ ------
+
+    pass # Remove this line
+
+    # ------ Place code above here /\ /\ /\ ------
+
+
+def exercise06(n):
+     # This function will receive an arbitrary list of numbers of arbitrary size and find the average of those numbers. The size of the list may vary. Find the method that requires the  least amount of code. Return back the length, sum of list and average of list
+
+    # ------ Place code below here \/ \/ \/ ------
+
+
+    # ------ Place code above here /\ /\ /\ ------
+    return length_n, sum_n, average_n
+
+
+def exercise07(n):
+    # This function looks for duplicates in list n. If there is a duplicate True is returned. If there are no duplicates False is returned.
+
+    # ------ Place code below here \/ \/ \/ ------
+
+
+    # ------ Place code above here /\ /\ /\ ------
+
+
+# ------ Place code below here \/ \/ \/ ------
+
+def exercise08(s):
+    # This function receives a string. The string should be casted to an int and then a float and returns each separately
+
+    return int_s, float_s
+
+# ------ Place code above here /\ /\ /\ ------
+
+def exercise09():
+    # Compile a list of 11 random URLs of dog pics. You will simply APPEND the dogs list with URLs of pics so that the list contains 11 URLs. This means you will extract the URL and insert into the list dogs.
+
+    dogs = []
+    url = 'https://random.dog/woof.json'
+    dog_media = r.get(url=url)
+    print(str(dog_media.content))
+    
+    # ------ Place code below here \/ \/ \/ ------
+    
+
+
+    # ------ Place code above here /\ /\ /\ ------
+
+    return dogs
+
+def exercise10(sentence):
+
+    # Exercise10 receives an arbitrary string. Return the sentence backwards with the cases inverted and spaces an underscore _, i.e. HelLo returns OlLEh
+    reversed = ''
+
+    # ------ Place code below here \/ \/ \/ ------
+    
+
+
+    # ------ Place code above here /\ /\ /\ ------
+    return reversed
+
+
+
+class TestAssignment2(unittest.TestCase):
+    def test_exercise01(self):
+        print('Testing exercise 1')
+        a = exercise01()
+        self.assertEqual(len(a), 5)
+        self.assertTrue('cat' in a)
+        self.assertTrue('dog' in a)
+        self.assertTrue('manta ray' in a)
+    
+    def test_exercise02(self):
+        print('Testing exercise 2')
+        a, l = exercise02()
+        self.assertEqual(len(a), 5)
+        self.assertEqual(l, 5)
+        self.assertTrue('cat' in a)
+        self.assertTrue('dog' in a)
+        self.assertTrue('manta ray' in a)
+
+    def test_exercise03(self):
+        print('Testing exercise 3')
+        c, tfe = exercise03()
+        self.assertEqual(c[0], 10)
+        self.assertEqual(c[11], -5)
+        self.assertEqual(len(c), 12)
+        self.assertEqual(tfe, 6)
+
+    def test_exercise04(self):
+        print('Testing exercise 4')
+        more_temperatures = np.random.randint(300, 400, size=25)
+        iot_sensor_points = {1: 801, 2: 644, 3: 991, 4: 721,
+                             5: 752, 6: 871, 7: 991, 8: 1023, 9: 804, 10: 882}
+        samples, temperatures, more_temperatures, iot_sensor_points, a, b, c, d, e, copy_of_samples = exercise04(more_temperatures, iot_sensor_points,
+                                                                                                                 8000, 8500, 9000, 9500, 9999)
+
+        self.assertEqual(len(temperatures), 50)
+        self.assertEqual(len(samples), 10)
+        self.assertEqual(temperatures[0], 9999)
+        self.assertEqual(temperatures[11], 801)
+        self.assertEqual(samples[9], 8000)
+        self.assertEqual(copy_of_samples[0], 8000)
+        self.assertEqual(a, 8000)
+        self.assertEqual(b, 8500)
+        self.assertEqual(c, 9000)
+        self.assertEqual(d, 9500)
+        self.assertEqual(e, 9999)
+
+    def test_exercise05(self):
+        print('Testing exercise 5')
+        self.assertEqual(exercise05(5), 120)
+        self.assertEqual(exercise05(10), 3628800)
+
+    def test_exercise06(self):
+        print('Testing exercise 6')
+        length_n, sum_n, average_n = exercise06([1, 2, 3, 4, 5])
+        self.assertEqual(average_n, 3)
+        self.assertEqual(length_n, 5)
+        length_n, sum_n, average_n = exercise06([1, 2, 120])
+        self.assertEqual(average_n, 41)
+        self.assertEqual(length_n, 3)
+
+    def test_exercise07(self):
+        print('Testing exercise 7')
+        self.assertTrue(exercise07([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == True)
+        self.assertTrue(exercise07([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == False)
+        self.assertTrue(exercise07([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]) == True)
+        self.assertTrue(exercise07([1, 2.00002, 2.00001, 4, 5, 6, 7, 8, 9, 10]) == False)
+    
+    def test_exercise09(self):
+        print('Testing exercise 9')
+        dogs = exercise09()
+        for d in dogs:
+            print(d)
+        self.assertTrue('https://random.dog/' in d)
+            
+
+    def test_exercise10(self):
+        print('Testing exercise 10')
+        self.assertEqual(exercise10('HellO'),'oLLEh')
+        self.assertEqual(exercise10('ThIs Is MaD'),'dAm_Si_SiHt')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
